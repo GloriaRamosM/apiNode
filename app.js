9be1ac286ee11d5f9ee0b29e16668afdf792bcf1
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-10-30 13:29:55 -0300

    servidor levantado

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 import express from "express";
 
 const app = express();
+const port = 3000;
 
-app.use(express.json());
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+
+app.get("/api/frase", (req, res) => {
+  res.send("Hola");
+});
+
+// Levantar el servidor
+const server = app.listen(port, () => console.log("Listening on PORT 3000"));
