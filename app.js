68cfcdc28e1d9a4e069fcf2c0717aeaa0ed42ff8
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-11-09 17:15:07 -0300

    agregue metodo post y metodo delete

--- app.js
+++ app.js
@@ -9,28 +9,47 @@ app.use(express.json()); // esto es para Interpretar el Req.body. Decodifica el 
 
 //Routes
 
-app.get("/api/frase", (req, res) => {
-  res.send("Hola");
+let users = [];
+//  GET
+app.get("/api/user", (req, res) => {
+  return res.status(200).send({ users });
 });
 
-app.get("api/users", (req, res) => {
-  res.send(
-    "Me tengo que traer todos los usuarios, estoy volviendo de a Poco si"
-  );
-});
-let users = [];
+//POST
 //El post siempre crea algo, siempre se usa el Body para recibir la data
 app.post("/api/user", (req, res) => {
   let user = req.body;
 
-  if (!user.name || !user.last_name) {
-    return res.status(400).send({ status: "error" });
+  if (!user.first_name || !user.last_name) {
+    return res
+      .status(400)
+      .send({ status: "error", error: "Datos incompletos" });
   }
 
   users.push(user);
 
-  return res.status(200).send({ status: "Sucess" });
+  return res.status(200).send({ status: "Sucess", message: "User created" }); // El status 200 viene implicito cuando no se indica
 });
 
+// PUT
+
+app.put("/api/user/:name", (req, res) => {
+  let name = req.params.name;
+});
+
+//DELETE
+
+app.delete("/api/user/:name", (req, res) => {
+  let name = req.params.name;
+
+  let currentLength = users.length;
+
+  users = users.filter((user) => user.first_name != name);
+  if (users.length === currentLength) {
+    return res.status(404).send({ status: "error", error: "User not found" });
+  }
+
+  res.send({ status: "sucess", message: "User deleted" });
+});
 // Levantar el servidor
 const server = app.listen(port, () => console.log("Listening on PORT 3000"));
